55e052a019dc14ee0bc678e58e8785d84be9f132
YamanaAnusha <anusha.yamana@gmail.com>
2019-05-24 13:51:18 +0530

    Update main.tf

--- chef/main.tf
+++ chef/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
+    access_key = "${var.accesskey}"
+    secret_key = "${var.secretkey}"
     region = "${var.region}"
 }
 resource "aws_instance" "web" {  
